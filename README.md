c19c5bce41c36452461f28bb6fa9164e83c77439
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-08-09 09:10:30 +0200

    initial commit with demo folder

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FilesortPS
+
+## SYNOPSIS
+A script to check for duplicate files in multiple directories.
+
+## DESCRIPTION
+Select folder with FolderBrowserDialog, then get all elements inside the folder and check the file hashes.
+If the hashes match, return the filenames that are a match.
+
+## NOTES
+Untested in PS 5.1
+Tested in PS 7.4.3
+Added test folder as example
+
+## How to run
+Download or clone the repo, find [./sort.ps1](./sort.ps1) and execute using Powershell.
+If the Filedialogue is not shown in foreground, check all opened windows with  `alt + tab`.
---
+++ sort.ps1
@@ -0,0 +1,77 @@
+<#
+.SYNOPSIS
+    A script to check for duplicate files in multiple directories.
+.DESCRIPTION
+    Select folder with FolderBrowserDialog, then get all elements inside the folder and check the file hashes.
+    If the hashes match, return the filenames that are a match.
+
+.NOTES
+    Untested in PS 5.1
+    Tested in PS 7.4.3
+    Added test folder as example
+.LINK
+    https://github.com/FabioTavernini/FilesortPS
+#>
+
+
+
+# Adding Types and defining Vars
+Add-Type -AssemblyName System.Windows.Forms
+
+
+function Compare-FileHashes {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]]$FilePaths
+    )
+
+    # Create a hashtable to store file hashes and corresponding file paths
+    $hashTable = @{}
+
+    foreach ($filePath in $FilePaths) {
+        if (Test-Path $filePath) {
+            # Compute the file hash
+            $hash = Get-FileHash -Path $filePath -Algorithm SHA256
+
+            # Store the file path under its hash in the hashtable
+            if ($hashTable.ContainsKey($hash.Hash)) {
+                $hashTable[$hash.Hash] += $filePath
+            }
+            else {
+                $hashTable[$hash.Hash] = @($filePath)
+            }
+        }
+        else {
+            Write-Host "File not found: $filePath" -ForegroundColor Red
+        }
+    }
+
+    # Return only the entries where more than one file path has the same hash
+    $matchingFiles = @()
+    foreach ($key in $hashTable.Keys) {
+        if ($hashTable[$key].Count -gt 1) {
+            $matchingFiles += [PSCustomObject]@{
+                Hash  = $key
+                Files = $hashTable[$key]
+            }
+        }
+    }
+    return $matchingFiles
+}
+
+$FileBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{ InitialDirectory = ".\" }
+$Null = $FileBrowser.ShowDialog()
+
+$selectedpath = $FileBrowser.SelectedPath
+
+$childelements = Get-ChildItem -Path $selectedpath -Recurse -Force | Select-Object LastWriteTime, Length, Name, FullName
+
+
+$multiples = $childelements | Group-Object Name | Where-Object { $_.Count -gt 1 }
+
+
+foreach ($multiple in $multiples) {
+
+    compare-Filehashes -FilePaths $multiple.Group.FullName
+
+}
